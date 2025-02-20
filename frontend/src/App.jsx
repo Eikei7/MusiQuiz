cdf30b7314d3247b93238dacca847ca7e39c5515
Erik Karlsson <69363348+Eikei7@users.noreply.github.com>
2025-02-20 15:49:55 +0100

    Simple login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,34 @@ function App() {
 
   return (
     <>
-      
+      <img src="/logo_big_2.png" alt="MusiQuiz logo" />
+            <div className="login-container">
+                <form className="login-form">
+                    <div className="input-group">
+                        <input 
+                            type="text" 
+                            id="email" 
+                            name="email"
+                            placeholder="Username (email address)" 
+                            required 
+                            autoComplete="username"
+                        />
+                    </div>
+                    <div className="input-group">
+                        <input 
+                            type="password" 
+                            id="password" 
+                            name="password"
+                            placeholder="Password"
+                            required 
+                            autoComplete="current-password"
+                        />
+                    </div>
+                    <div className="input-group">
+                        <button type="submit">Let's GO!</button>
+                    </div>
+                </form>
+            </div>
     </>
   )
 }
