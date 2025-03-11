105aa8da38a6516cffa6520574237db83dfc7946
Erik Karlsson <erikmatfors@gmail.com>
2025-03-11 14:34:00 +0100

    Background gradient animation

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Login-Signup.css';
-import { ENDPOINT_LOGIN } from './endpoints';
 import { useAuth } from './AuthContext';
 
 function Login() {
